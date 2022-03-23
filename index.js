37a3a6c96588ab3851c96cb7a8d7e997195b09d3
MaximoEspitia <max19espitia@gmail.com>
2022-03-23 18:22:20 -0500

    commit tree d9a7bc5bc4224e35af5985b6398bc92fffbf27b7

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('main').remove();
+//let const name = new type(arguments);
+//const newHeader = 'h1#victory';
+//newHeader.append('h1#victroy');
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Max Espitia is the champion"
+document.body.appendChild(newHeader)
+
